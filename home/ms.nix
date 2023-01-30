1334c04918b47fb285899a50cd4b7b2ce4dd1f6a
Marc Singer <ms@pr0.tech>
2023-01-30 09:43:52 +0100

    Updated the flake lock file @1675068230

--- home/ms.nix
+++ home/ms.nix
@@ -9,7 +9,7 @@ in
 
   home.stateVersion = "22.11";
 
-  home.file.".background-image".source = ./. + "/wallpaper/${hostname}";
+  home.file.".background-image".source = ./. + "/wallpaper/" + "${hostname}";
 
   programs.home-manager.enable = true;
   
