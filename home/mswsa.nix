f628f2654933aca68116ce674906ba010ff9f7cb
Marc Singer <ms@pr0.tech>
2023-01-31 06:49:23 +0100

    Updated the flake lock file @1675144161

--- home/mswsa.nix
+++ home/mswsa.nix
@@ -1,7 +1,10 @@
 { config, lib, pkgs, user, ... }:
 
 {
-  home.gtk.cursorTheme = "capitaine-cursors";
+  home.misc.gtk = {
+    cursorTheme =  "capitaine-cursors";
+    cursorTheme.package = pkgs.gnome-capitaine-cursors;
+  };
 
   home.file.".config/wall".source = ./wallpaper/mswsa;
   home.file.".background-image".source = ./wallpaper/mswsa;
