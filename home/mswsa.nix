ccafa85e0ecc54784a3a337698c49b2c93dfa4f9
Marc Singer <ms@pr0.tech>
2023-02-02 07:28:36 +0100

    Updated the flake lock file @1675319315

--- home/mswsa.nix
+++ home/mswsa.nix
@@ -1,6 +1,14 @@
 { config, lib, pkgs, user, ... }:
 
 {
+
+  services.syncthing = {
+    enable = true;
+    user = "ms";
+    dataDir = "/home/ms/Documents";
+    configDir = "/home/ms/.config/syncthing";
+  };
+
   home.file.".background-image".source = ./wallpaper/mswsa;  # Wallpaper Link
   gtk.cursorTheme.size = 32;  # Scale size with display resolution
 }
