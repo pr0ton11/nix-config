a6d8f240bcf65f06771c140b0af560a524f6e5a8
Marc Singer <ms@pr0.tech>
2023-02-26 20:01:20 +0100

    Updated the flake lock file @1677438078

--- home/mswsa.nix
+++ home/mswsa.nix
@@ -3,5 +3,5 @@
 {
   home.file.".background-image".source = ./wallpaper/mswsa;  # Wallpaper Link
   gtk.cursorTheme.size = 32;  # Scale size with display resolution
-  users.users.ms.extraGroups = [ "libvirtd" ];
+  extraGroups = [ "libvirtd" ];
 }
