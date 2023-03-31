2bfe3ea0f4e42e2f83666e788c9b658c607889af
Marc Singer <ms@pr0.tech>
2023-03-31 21:22:38 +0200

    Updated the flake lock file @1680290537

--- machine/mswsa.nix
+++ machine/mswsa.nix
@@ -66,6 +66,7 @@
   hardware.opengl.driSupport = true;
   hardware.opengl.driSupport32Bit = true;
   hardware.steam-hardware.enable = true;  # Enable steam udev profiles for controllers
+  hardware.opengl.mesaPackage = pkgs.mesa_23;
 
   # Lutris installation
   # Flatpak lutris does not work
